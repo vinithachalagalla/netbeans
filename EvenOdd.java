33814ed10c1a491385337de362e4c7bd6dd3cad7
vinithachalagalla <92858793+vinithachalagalla@users.noreply.github.com>
2021-12-02 10:54:06 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author student
+ */
+public class EvenOdd {
+    public static void main(String args[]){
+        int n=35;
+        if (n%2==0)
+        {
+            System.out.println("Even Number");
+        }
+        else
+        {
+            System.out.println("Odd Number");
+        }
+    }
+    
+}
