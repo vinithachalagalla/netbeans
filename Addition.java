d321825d17c2cf8a4749b6282ec62ff42474a1c1
vinithachalagalla <92858793+vinithachalagalla@users.noreply.github.com>
2021-12-02 10:59:45 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author student
+ */
+public class Addition {
+    public static void main(String args[]){
+        int a=25,b=10,sum=0;
+        sum=a+b;
+        System.out.println("Addition of two numbers is:"+sum);
+    }
+    
+}
