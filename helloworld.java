3155aacb6ba55c9c2562a671a900dccc8cf270d8
vinithachalagalla <92858793+vinithachalagalla@users.noreply.github.com>
2021-12-02 11:00:20 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author student
+ */
+public class helloworld {
+    public static void main(String args[]){
+        System.out.println("welcome to netbeans");
+        System.out.println("hello world program");
+    }
+    
+}
